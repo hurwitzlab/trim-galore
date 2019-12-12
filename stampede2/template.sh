4eff06bb111ef8b289fb2a7fbecf008341bdfd81
Ken Youens-Clark <kyclark@email.arizona.edu>
2019-12-12 12:31:41 -0600

    new template

--- stampede2/template.sh
+++ stampede2/template.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+IMG="/work/05066/imicrobe/singularity/trim-galore-0.6.5.img"
 
-echo "QUERY            \"${QUERY}\""
-echo "ALIAS_FILE       \"${ALIAS_FILE}\""
-echo "EUC_DIST_PERCENT \"${EUC_DIST_PERCENT}\""
-echo "SAMPLE_DIST      \"${SAMPLE_DIST}\""
-echo "NUM_SCANS        \"${NUM_SCANS}\""
-echo "METADATA_FILE    \"${METADATA_FILE}\""
+if [[ ! -e "$IMG" ]]; then
+    echo "Missing Singularity image \"$IMG\""
+    exit 1
+fi
 
-sh run.sh ${QUERY} ${ALIAS_FILE} ${EUC_DIST_PERCENT} ${SAMPLE_DIST} ${NUM_SCANS} ${METADATA_FILE}
+singularity exec $IMG run_trim_galore -o "trim-galore-out" ${QUERY} ${CONSIDER_ALREADY_TRIMMED} ${DONT_GZIP} ${GZIP} ${ILLUMINA} ${KEEP} ${LENGTH_1} ${LENGTH_2} ${NEXTERA} ${NON_DIRECTIONAL} ${PHRED64} ${RBBS} ${RETAIN_UNPAIRED} ${SMALL_RNA} ${TRIM} ${TRIM_N} ${ADAPTER} ${ADAPTER2} ${BASENAME} ${CLIP_R1} ${CLIP_R2} ${ERROR_RATE} ${HARDTRIM3} ${HARDTRIM5} ${LENGTH} ${MAX_LENGTH} ${MAX_N} ${NEXTSEQ} ${NUM_CONCURRENT_JOBS} ${NUM_HALT} ${QUALITY} ${STRINGENCY} ${THREE_PRIME_CLIP_R1} ${THREE_PRIME_CLIP_R2}
+
+echo "Comments to kyclark@email.arizona.edu"
